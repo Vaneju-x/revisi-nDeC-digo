65414246f14ca4afe421906b661352d1aef97a30
Vanessa Contreras <vanessaj.contreras.n@Outlook.com>
2024-10-11 18:52:49 -0600

    fix: se reqlizaron algunas correcciones al codigo de eduardo

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,19 +4,27 @@ const $n = document.querySelector('name');
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
+async function displayUser(username) {/* sew agrega async */
+  try{ /* se añade try y catch */
+    $n.textContent = 'cargando...';
+  const response =  await fetch(`${usersEndpoint}/${username}`); 
+  /*se define data   */
+  const data = await response.json();
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;//cambio de comillas
+  $b.textContent = `${data.blog}`; //cambio de comillas
+  $l.textContent = `${data.location}`;//cambio de comillas
+}catch (err) {
+  handleError(err);
+}
+
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+ $n.textContent = `Algo salió mal: ${err}`;//se coloca ; , y el $
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski');
+/* .catch(handleError) lo retiramos  */
